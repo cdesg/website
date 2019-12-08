904516bf43e2b072975e11fc3324f62bf3475e44
Neil Mispelaar <neil.mispelaar@outlook.com>
2019-12-07 21:55:50 -0500

    Initial commit (version 2)

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,14 @@
+/* Include the PurgeCSS plugin */ 
 const purgecss = require('@fullhuman/postcss-purgecss')
 
+
 module.exports = {
-
 	plugins: [
-	  // ...
-	  require('tailwindcss'),
-
-	  purgecss({
-		content: ['./**/*.html']
-	  })
-	  // ...
+		require('tailwindcss'),
+		require('autoprefixer'),
+		purgecss({
+			content: ['./layouts/**/*.html', './contents/**/*.html'],
+			defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+		  })
 	]
-
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 // tailwind.config.js
 module.exports = {
+	theme: {
+		extend: {
+			colors: {
+				'brand-orange': '#BE1E2D',  
+			} 
+		}
+	},
+	variants: {},
+
 	plugins: [
-	  //require('tailwindcss-transforms'),
-	  //require('tailwindcss-transitions'),
-	  //require('tailwindcss-border-gradients'),
+		//require('tailwindcss-transforms'),
+		//require('tailwindcss-transitions'),
+		//require('tailwindcss-border-gradients'),
 	],
-  }
+}
