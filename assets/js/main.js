b476b6254b28462c5b9a4b53f2ef15b2d12199ce
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-03-13 14:02:22 -0400

    Remove menu plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,13 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     // Our hawaiian greeting is displayed as soon as the page loads,
-
     console.log('Aloha');
 
 	// Allows you to keep most of your variables and functions out of 
 	// the global scope, but make some of them publicly available.
-	menu.init({ 
-		selector: '#menu'
-	});
+	// menu.init({ 
+	//   selector: '#menu'
+	// });
 
 	
 
