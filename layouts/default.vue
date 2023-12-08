4d673102dab102227335da6ae403786eab64800c
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-08 05:36:23 -0500

    initial nuxt project

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import appHeader from '@/components/app-header.vue';
+import appFooter from '@/components/app-footer.vue';
+import appPage from '@/components/app-page.vue';
+</script>
+
+<template>
+  <app-page>
+    <template #header>
+      <app-header />
+    </template>
+    <slot />
+
+    <template #footer>
+      <app-footer />
+    </template>
+  </app-page>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <landing-hero />
+  <landing-metrics />
+  <landing-councillors />
+  <landing-statistics />
+  <landing-facts />
+</template>
+
+<style>
+/* No custom styles needed */
+</style>
