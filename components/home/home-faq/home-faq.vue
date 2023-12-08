6459a8981c147e990b987f0179cc75f86e252369
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-08 06:57:37 -0500

    Add lorem

--- components/home/home-faq/home-faq.vue
+++ components/home/home-faq/home-faq.vue
@@ -13,7 +13,7 @@ const faqs = [
   {
     question: 'How do you make holy water?',
     answer:
-      'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, saepe.',
   },
   // More questions...
 ];
