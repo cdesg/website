d5afe6d279c7df1a09d2ceed1779a09cf70b822a
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-09 18:16:20 -0500

    Update header

--- components/app/app-header/app-header.vue
+++ components/app/app-header/app-header.vue
@@ -2,35 +2,34 @@
 </script>
 
 <template>
-  <div class="z-50">
-    <header>
-      <u-container>
-        <div class="flex items-center justify-between py-6 lg:py-8">
-          <h2 class="sr-only">
-            Site header
-          </h2>
+  <header>
+    <!-- Elevate the container above the hero because of the background blur -->
+    <u-container class="z-50">
+      <div class="flex items-center justify-between py-6 lg:py-12">
+        <h2 class="sr-only">
+          Site header
+        </h2>
 
-          <nav class="flex lg:flex-1">
-            <h3 class="sr-only">
-              Site navigation
-            </h3>
-            <!-- Site logo -->
-            <nuxt-link
-              to="/"
-              class="flex items-center"
+        <nav class="flex lg:flex-1">
+          <h3 class="sr-only">
+            Site navigation
+          </h3>
+          <!-- Site logo -->
+          <nuxt-link
+            to="/"
+            class="flex items-center"
+          >
+            <!-- Convert this to SVG and text -->
+            <img
+              class="h-12 sm:h-16"
+              src="/images/logo/brand.png"
+              alt="Logo for the CDESG"
             >
-              <!-- Convert this to SVG and text -->
-              <img
-                class="h-12 sm:h-16"
-                src="/images/logo/brand.png"
-                alt="Logo for the CDESG"
-              >
-            </nuxt-link>
-          </nav>
-        </div>
-      </u-container>
-    </header>
-  </div>
+          </nuxt-link>
+        </nav>
+      </div>
+    </u-container>
+  </header>
 </template>
 
 <style>
