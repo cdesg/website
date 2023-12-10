2c1d00b3c70410d3c4cf271006edf27fde35f7a2
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-10 14:54:02 -0500

    Update partners section

--- components/home/home-partners/home-partners.vue
+++ components/home/home-partners/home-partners.vue
@@ -2,11 +2,19 @@
   <section>
     <u-container>
       <div class="py-24 sm:py-32">
-        <div class="mx-auto max-w-2xl lg:max-w-none">
-          <h2 class="text-lg font-semibold leading-8 text-gray-900">
-            Trusted by the world's most innovative teams
+        <div class="max-w-4xl mx-auto">
+          <h2
+            class="
+          text-center
+          text-3xl
+          font-bold
+          tracking-tight
+          text-gray-900
+          sm:text-5xl"
+          >
+            Current sponsorships and partners for our workshops and speakers.
           </h2>
-          <div class="mx-auto mt-10 grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-5">
+          <div class="mx-auto mt-20 grid grid-cols-4 items-start gap-x-8 gap-y-10 sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:grid-cols-3 opacity-75">
             <img
               class="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
               src="https://tailwindui.com/img/logos/transistor-logo-gray-900.svg"
@@ -39,6 +47,13 @@
               class="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
               src="https://tailwindui.com/img/logos/statamic-logo-gray-900.svg"
               alt="Statamic"
+              width="158"
+              height="48"
+            >
+            <img
+              class="col-span-2 max-h-12 w-full object-contain object-left lg:col-span-1"
+              src="https://tailwindui.com/img/logos/transistor-logo-gray-900.svg"
+              alt="Transistor"
               width="158"
               height="48"
             >
