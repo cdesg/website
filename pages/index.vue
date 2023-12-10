9d725e1a668e6df1fdef502453b98d33382c81c6
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-09 22:45:41 -0500

    Remove unused home sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,13 @@
 <script setup lang="ts">
 import homeHero from '@/components/home/home-hero/home-hero.vue';
-import homeMission from '@/components/home/home-mission/home-mission.vue';
+import homeAbout from '@/components/home/home-about/home-about.vue';
 import homeMetrics from '@/components/home/home-metrics/home-metrics.vue';
-import homeFaq from '@/components/home/home-faq/home-faq.vue';
 </script>
 
 <template>
   <home-hero />
-  <home-mission />
+  <home-about />
   <home-metrics />
-  <home-faq />
 </template>
 
 <style>
