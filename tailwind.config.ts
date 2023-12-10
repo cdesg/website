a1700b6ccca58712382e1d5e54c34fdabff5df55
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-09 22:23:54 -0500

    Start doing some theme configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,19 @@ export default <Partial<Config>> {
   theme: {
     extend: {
       colors: {
-        'brand-accent': '#be1e2d',
+        red: {
+          50: '#fef2f3',
+          100: '#fee2e5',
+          200: '#fecacf',
+          300: '#fba6ae',
+          400: '#f7727e',
+          500: '#ee4555',
+          600: '#db2738',
+          700: '#be1e2d',
+          800: '#981c28',
+          900: '#7e1e27',
+          950: '#450a10',
+        },
       },
     },
   },
