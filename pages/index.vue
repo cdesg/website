04dbdcb74841ad78353bdc5ebee66902947870ca
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-10 15:32:56 -0500

    Remove home metrics and update home events

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import homeHero from '@/components/home/home-hero/home-hero.vue';
 import homeAbout from '@/components/home/home-about/home-about.vue';
-import homeMetrics from '@/components/home/home-metrics/home-metrics.vue';
 import homeWhatWeDo from '@/components/home/home-what-we-do/home-what-we-do.vue';
 import homePartners from '@/components/home/home-partners/home-partners.vue';
+import homeEvents from '@/components/home/home-events/home-events.vue';
 </script>
 
 <template>
   <home-hero />
   <home-about />
   <home-what-we-do />
-  <home-metrics />
+  <home-events />
   <home-partners />
 </template>
 
