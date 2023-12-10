a105c3ef02527193c16f3e8924cfc4f7806dc780
Neil Mispelaar <neil.mispelaar@outlook.com>
2023-12-09 21:16:29 -0500

    Update hero

--- components/home/home-hero/home-hero.vue
+++ components/home/home-hero/home-hero.vue
@@ -2,10 +2,12 @@
 </script>
 
 <template>
+  <!-- Hero container -->
   <div class="relative isolate pt-14">
-    <div class="absolute inset-0 flex items-center justify-center -z-10 transform-gpu overflow-hidden blur-3xl opacity-40">
+    <!-- Floating Blob -->
+    <div class="absolute inset-1 flex justify-center items-center -z-10 transform-gpu overflow-hidden opacity-40 blur-3xl">
       <svg
-        class="w-[950px] fill-current text-brand-accent/30 mt-10 ml-60"
+        class="w-[1050px] fill-current text-brand-accent/30"
         viewBox="0 0 200 200"
         xmlns="http://www.w3.org/2000/svg"
       >
@@ -16,23 +18,26 @@
         />
       </svg>
     </div>
-    <div class="py-24 sm:py-28 lg:pb-40">
-      <div class="mx-auto max-w-5xl px-6 lg:px-8">
-        <div class="mb-8 flex justify-start">
-          <a
-            class="inline-block px-3 py-1 text-gray-900 text-sm ring-1 ring-red-900 hover:ring-brand-accent rounded-full leading-6"
-            href="#"
-          >
-            Lorem ipsum dolor sit amet
-            <span class="font-bold text-brand-accent">
-              Read more <span aria-hidden="true">&rarr;</span>
-            </span>
-          </a>
-        </div>
-        <div class="mx-auto max-5-4xl">
-          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-7xl">
+    <u-container>
+      <div class="py-24 sm:py-28 lg:pb-40">
+        <div class="max-w-5xl lg:max-w-6xl xl:max-w-7xl">
+          <!-- Mini Action Bar -->
+          <div class="mb-8 flex justify-start">
+            <a
+              class="inline-block px-3 py-1 text-gray-900 text-sm ring-1 ring-brand-accent hover:ring-red-900 rounded-full leading-6 bg-neutral-50"
+              href="#"
+            >
+              Lorem ipsum dolor sit amet
+              <span class="font-bold text-brand-accent">
+                Read more <span aria-hidden="true">&rarr;</span>
+              </span>
+            </a>
+          </div>
+          <!-- Header Title -->
+          <h1 class="text-4xl font-bold tracking-tight text-neutral-900 sm:text-7xl xl:text-8xl">
             Dedicated to increasing the visibility of development economics in Canada.
           </h1>
+          <!-- Button row -->
           <div class="mt-10 flex items-center justify-start gap-x-6">
             <a
               href="#"
@@ -41,65 +46,6 @@
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  <div
-    v-if="false"
-    class="relative isolate overflow-hidden bg-gray-950 "
-  >
-    <div
-      class="absolute left-[calc(50%-4rem)] top-10 -z-10 transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(70%-30rem)] xl:left-[calc(50%-24rem)]"
-      aria-hidden="true"
-    >
-      <div
-        class="aspect-[1108/632] w-[69.25rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-20"
-        style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"
-      />
-    </div>
-    <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-48">
-      <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-0 ">
-        <div class="mt-24 sm:mt-32 lg:mt-16">
-          <a
-            href="#"
-            class="inline-flex space-x-6"
-          >
-            <span class="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-400 ring-1 ring-inset ring-indigo-500/20">What's new</span>
-            <span class="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
-              <span>Just shipped v1.0</span>
-            </span>
-          </a>
-        </div>
-        <h1 class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">
-          Deploy to the cloud with confidence
-        </h1>
-        <p class="mt-6 text-lg leading-8 text-gray-300 ">
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
-        </p>
-        <div class="mt-10 flex items-center gap-x-6">
-          <a
-            href="#"
-            class="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400"
-          >Get started</a>
-          <a
-            href="#"
-            class="text-sm font-semibold leading-6 text-white"
-          >Learn more <span aria-hidden="true">→</span></a>
-        </div>
-      </div>
-      <div
-        v-if="false"
-        class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32"
-      >
-        <div class="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-          <img
-            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-            alt="App screenshot"
-            width="2432"
-            height="1442"
-            class="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10"
-          >
-        </div>
-      </div>
-    </div>
+    </u-container>
   </div>
 </template>
